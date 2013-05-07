9f3f2037d05b20376cded6c1d064625ee83ce4bc
Per Sandstrom <per.sandstrom@sogeti.se>
2013-05-07 16:57:40 +0200

    now uses object. Also added distance to allies and stuff

--- js/planet.js
+++ js/planet.js
@@ -2,41 +2,59 @@
     "use strict";
     var ploxworld = window.ploxworld = window.ploxworld || {};
 
+    //support methods:
     ploxworld.getRandomPlanet = function () {
         return ploxworld.planets[Math.floor(Math.random() * ploxworld.planets.length)];
     };
 
-    ploxworld.makePlanet = function (objectName, x, y) {
-//        console.log("objectName: " + objectName);
-//        console.log("x: " + x);
+    /**
+     * calculates the "closestAllied" for all planets, call when allied stuff changes
+     */
+    ploxworld.calculateTradeMap = function () {
+        console.log("calculateTradeMap");
 
-        var planet = function planet() {
-            planet.objectName = objectName;
-            planet.x = x;
-            planet.y = y;
-            planet.pop = 100;
+        $.each(ploxworld.planets, function (index, planet) {
+            planet.closestAllied.length = 0;
+            $.each(ploxworld.planets, function (index, otherPlanet) {
+                //TODO check that the planet is allied
+                if (planet !== otherPlanet) {
+                    planet.closestAllied.push(otherPlanet);
+                }
+            });
+            planet.closestAllied.sort(function closest(a, b) {
+                return planet.planetDistance[a.objectName] - planet.planetDistance[b.objectName];
+            });
 
-            //supply
-            //supplyProd
-
-            planet.planetDistance = {};
-
-            planet.tic = function () {
-                console.log("tic: " + planet.pop);
-
-                planet.pop = planet.pop * 1.0001;
-            };
+            console.log(planet.objectName + " closest allies: ");
+            $.each(planet.closestAllied, function (index, closesAllied) {
+                console.log(closesAllied.objectName);
+            });
+        });
 
 
-            planet.setPlanetDistance = function (planet, distance) {
-                //TODO can I remove "planet."?
-                planet.planetDistance[planet] = distance;
-            };
+    };
 
-            return planet;
-        }();
+    //object:
+    ploxworld.Planet = function Planet(objectName, x, y) {
+        this.objectName = objectName;
+        this.x = x;
+        this.y = y;
+        this.pop = 100;
+        this.planetDistance = {};   //planetName -> distance
+        this.closestAllied = [];    //closest allied planets, to evaluate where trade goes
+    };
 
-        return planet;
+    var Planet = ploxworld.Planet;
+
+    Planet.prototype.tic = function () {
+        console.log("tic: " + this.pop);
+        this.pop = this.pop * 1.0001;
+    };
+
+    Planet.prototype.setPlanetDistance = function (planet, distance) {
+//        console.log(this.objectName + " distance to " + planet.objectName + " is " + distance);
+        this.planetDistance[planet.objectName] = distance;
+//        console.log("result: " + this.planetDistance[planet.objectName]);
     };
 
 })();
--- js/planetGenerator.js
+++ js/planetGenerator.js
@@ -13,10 +13,10 @@
             return name;
         };
 
-        ploxworld.planets = [ploxworld.makePlanet(takeFreeName(), 200, 250),
-            ploxworld.makePlanet(takeFreeName(), 50, 350),
-            ploxworld.makePlanet(takeFreeName(), 150, 350),
-            ploxworld.makePlanet(takeFreeName(), 300, 100)
+        ploxworld.planets = [new ploxworld.Planet(takeFreeName(), 200, 250),
+            new ploxworld.Planet(takeFreeName(), 50, 350),
+            new ploxworld.Planet(takeFreeName(), 150, 350),
+            new ploxworld.Planet(takeFreeName(), 305, 100)
         ];
 
         //calculate cost of travel
@@ -28,12 +28,24 @@
                 var yDiff = Math.abs(planet1.y - planet2.y);
                 var realDistance = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
                 var costDistance = Math.pow(realDistance, 1.1);
-//                console.log(i + " to " + j + " distance: " + realDistance);
-//                console.log(i + " to " + j + " cost: " + costDistance);
+//                console.log(planet1.objectName + " to " + planet2.objectName + " distance: " + realDistance);
+//                console.log(planet1.objectName + " to " + planet2.objectName + " cost: " + costDistance);
                 planet1.setPlanetDistance(planet2, costDistance);
                 planet2.setPlanetDistance(planet1, costDistance);
             }
         }
+
+
+//        for (i = 0; i < ploxworld.planets.length; i++) {
+//            var planet = ploxworld.planets[i];
+//            console.log("iterating " + planet.objectName);
+//            for (var otherPlanetName in  planet.planetDistance) {
+//                console.log(planet.objectName + " to " + otherPlanetName + " is " + planet.planetDistance[otherPlanetName]);
+//            }
+//        }
+
+        //calculate initial trade map:
+        ploxworld.calculateTradeMap();
 
         return ploxworld.planets;
     };
